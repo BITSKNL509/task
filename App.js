05457cf189ac8a7e7afedefcddc1308237739f93
BITSKNL509 <davinisanthoshkumar362@gmail.com>
2025-06-09 18:04:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './counter';
+import Cal from './cal';
+function App() {
+  return (
+    <div className="App">
+      <Counter/>
+      <Cal/>
+    </div>
+  );
+}
+
+export default App;
